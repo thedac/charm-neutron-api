95df1deddeae38d64a259106c982944e189431c6
Edward Hope-Morley <edward.hope-morley@canonical.com>
2014-09-30 14:49:52 +0100

    fixed ipv6 compatibility check

--- hooks/neutron_api_utils.py
+++ hooks/neutron_api_utils.py
@@ -227,9 +227,9 @@ def migrate_neutron_database():
 
 
 def setup_ipv6():
-    ubuntu_rel = float(lsb_release()['DISTRIB_RELEASE'])
-    if ubuntu_rel < 'trusty':
-        raise Exception("IPv6 is not supported for Ubuntu "
+    ubuntu_rel = lsb_release()['DISTRIB_CODENAME'].lower()
+    if ubuntu_rel < "trusty":
+        raise Exception("IPv6 is not supported in the charms for Ubuntu "
                         "versions less than Trusty 14.04")
 
     # NOTE(xianghui): Need to install haproxy(1.5.3) from trusty-backports
