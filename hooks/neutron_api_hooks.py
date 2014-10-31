dbe897e5b38b9f56dd412b381bc963f0ef4e6608
Brad Marshall <brad.marshall@canonical.com>
2014-10-31 14:51:44 +1000

    [bradm] Added support to get nagios hostname from nrpe relation

--- hooks/neutron_api_hooks.py
+++ hooks/neutron_api_hooks.py
@@ -14,6 +14,7 @@ from charmhelpers.core.hookenv import (
     relation_get,
     relation_ids,
     relation_set,
+    relations_of_type,
     open_port,
     unit_get,
 )
@@ -374,7 +375,13 @@ def ha_changed():
 
 @hooks.hook('nrpe-external-master-relation-joined', 'nrpe-external-master-relation-changed')
 def update_nrpe_config():
-    nrpe = NRPE()
+    # Find out if nrpe set nagios_hostname
+    hostname = None
+    for rel in relations_of_type('nrpe-external-master'):
+        if 'nagios_hostname' in rel:
+            hostname = rel['nagios_hostname']
+            break
+    nrpe = NRPE(hostname=hostname)
     apt_install('python-dbus')
     
     nrpe.add_check(
