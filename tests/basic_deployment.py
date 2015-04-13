03bdfbae23802ed687d972707d4d61b65fb9898b
Corey Bryant <corey.bryant@canonical.com>
2015-04-13 12:10:28 +0000

    Get http_proxy from AMULET_HTTP_PROXY env var

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import amulet
+import os
 import yaml
 
 from charmhelpers.contrib.openstack.amulet.deployment import (
@@ -71,6 +72,7 @@ class NeutronAPIBasicDeployment(OpenStackAmuletDeployment):
         neutron_api_config = {}
         if self.git:
             branch = 'stable/' + self._get_openstack_release_string()
+            amulet_http_proxy = os.environ.get('AMULET_HTTP_PROXY')
             openstack_origin_git = {
                 'repositories': [
                     {'name': 'requirements',
@@ -81,8 +83,8 @@ class NeutronAPIBasicDeployment(OpenStackAmuletDeployment):
                      'branch': branch},
                 ],
                 'directory': '/mnt/openstack-git',
-                'http_proxy': 'http://squid.internal:3128',
-                'https_proxy': 'https://squid.internal:3128',
+                'http_proxy': amulet_http_proxy,
+                'https_proxy': amulet_http_proxy,
             }
             neutron_api_config['openstack-origin-git'] = yaml.dump(openstack_origin_git)
         keystone_config = {'admin-password': 'openstack',
