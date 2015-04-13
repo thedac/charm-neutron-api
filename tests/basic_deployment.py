c4be5839a026378032978f57a2857ba37e2254ee
Corey Bryant <corey.bryant@canonical.com>
2015-04-13 19:17:31 +0000

    Turn DEBUG on by default for amulet tests

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -15,7 +15,7 @@ from charmhelpers.contrib.openstack.amulet.utils import (
 )
 
 # Use DEBUG to turn on debug logging
-u = OpenStackAmuletUtils(ERROR)
+u = OpenStackAmuletUtils(DEBUG)
 
 
 class NeutronAPIBasicDeployment(OpenStackAmuletDeployment):
