7819cf768789bc1987939c65a0dc8970bc60aedf
Corey Bryant <corey.bryant@canonical.com>
2015-04-22 18:40:12 +0000

    Add libffi-dev, libssl-dev, and libyaml-dev to base git install packages

--- hooks/neutron_api_utils.py
+++ hooks/neutron_api_utils.py
@@ -66,8 +66,11 @@ KILO_PACKAGES = [
 ]
 
 BASE_GIT_PACKAGES = [
+    'libffi-dev',
+    'libssl-dev',
     'libxml2-dev',
     'libxslt1-dev',
+    'libyaml-dev',
     'python-dev',
     'python-pip',
     'python-setuptools',
