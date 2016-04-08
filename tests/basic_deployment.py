47062d76f358ab6f788d33f9590aa01a4628bfcf
James Page <james.page@ubuntu.com>
2016-04-08 11:40:48 +0100

    Check for Keystone apache2 process for liberty+
    The keystone charm recently changed to run keystone as a wsgi
    process under Apache2; refactor amulet test to ensure that
    apache2 is checked instead of keystone for >= liberty.
    
    Change-Id: Ib1cad4aa8be253cf3d44f844322f90c944baeb76

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -216,6 +216,9 @@ class NeutronAPIBasicDeployment(OpenStackAmuletDeployment):
             self.neutron_api_sentry: neutron_api_services,
         }
 
+        if self._get_openstack_release() >= self.trusty_liberty:
+            services[self.keystone_sentry] = ['apache2']
+
         ret = u.validate_services_by_name(services)
         if ret:
             amulet.raise_status(amulet.FAIL, msg=ret)
