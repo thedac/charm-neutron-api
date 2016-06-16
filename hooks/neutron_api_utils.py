5251e366b404867d9e3cf3ee915f5cfae3c6ba8e
Corey Bryant <corey.bryant@canonical.com>
2016-06-16 13:01:13 +0000

    Add defaults for openstack-origin-git config option
    openstack-origin-git currently only supports YAML that specifies
    the git repositories to deploy from.
    
    This adds support for default openstack-origin-git values. The
    default values supported are: icehouse, kilo, liberty, mitaka,
    and master.  For example: openstack-origin-git=master.
    
    Change-Id: I9e20d58b85d14b830e0394b8603ee018da86f004

--- hooks/neutron_api_utils.py
+++ hooks/neutron_api_utils.py
@@ -16,6 +16,7 @@ from charmhelpers.contrib.openstack.utils import (
     get_os_codename_install_source,
     git_install_requested,
     git_clone_and_install,
+    git_default_repos,
     git_src_dir,
     git_pip_venv_dir,
     git_yaml_value,
@@ -568,6 +569,7 @@ def git_install(projects_yaml):
     """Perform setup, and install git repos specified in yaml parameter."""
     if git_install_requested():
         git_pre_install()
+        projects_yaml = git_default_repos(projects_yaml)
         git_clone_and_install(projects_yaml, core_project='neutron')
         git_post_install(projects_yaml)
 
