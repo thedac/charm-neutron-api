85c5af4d0b5a9d7ef7e7a946430e964e485bc3c9
David Ames <david.ames@canonical.com>
2016-12-15 12:04:05 -0800

    Add modern NSX support
    TODO: Fix sub config options like dvs_id etc
    
    Change-Id: I3735325c6b717395e4af7e1859de5b9895aa86d3

--- hooks/charmhelpers/contrib/openstack/neutron.py
+++ hooks/charmhelpers/contrib/openstack/neutron.py
@@ -251,6 +251,8 @@ def neutron_plugins():
         plugins['nsx']['server_packages'].remove('neutron-plugin-vmware')
         plugins['nsx']['server_packages'].append('python-vmware-nsx')
         plugins['nsx']['config'] = '/etc/neutron/nsx.ini'
+        plugins['nsx']['driver'] = (
+            'vmware_nsx.plugin.{}'.format(config('nsx-plugin')))
         plugins['vsp']['driver'] = (
             'nuage_neutron.plugins.nuage.plugin.NuagePlugin')
     return plugins
